86b98ea8e2dc131972bfa47fddd13fa73621e1f7
Jake Clifford <jakeclifford35@gmail.com>
2022-04-06 13:51:18 +1000

    skill-selected class

--- src/componants/about/About.jsx
+++ src/componants/about/About.jsx
@@ -18,18 +18,19 @@ export default function About() {
 
     return (
         <div className="about" id="about">
+            <h1>SKILLS</h1>
             <div className="skills">
-                <div className="skill" onClick={() => setDisplay(Frontend)}>
-                    <button>Frontend</button>
+                <div className={display == Frontend ? "skill skill-selected" : "skill"} onClick={() => setDisplay(Frontend)}>
+                    <h2>Frontend</h2>
                 </div>
-                <div className="skill" onClick={() => setDisplay(Ui)}>
-                    <button>UI/UX</button>
+                <div className={display == Ui ? "skill skill-selected" : "skill"} onClick={() => setDisplay(Ui)}>
+                    <h2>UI/UX</h2>
                 </div>
-                <div className="skill" onClick={() => setDisplay(Blockchain)}>
-                    <button>Web3</button>
+                <div className={display == Blockchain ? "skill skill-selected" : "skill"} onClick={() => setDisplay(Blockchain)}>
+                    <h2>Web3</h2>
                 </div>
-                <div className="skill" onClick={() => setDisplay(Cs)}>
-                    <button>Computer Science</button>
+                <div className={display == Cs ? "skill skill-selected" : "skill"} onClick={() => setDisplay(Cs)}>
+                    <h2>CS</h2>
                 </div>
             </div>
             <div className="display">
